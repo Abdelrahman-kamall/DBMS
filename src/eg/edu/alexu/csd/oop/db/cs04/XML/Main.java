521e09f486006951fb951bebf9719c8b4b591f8c
Abdelfattah Mohamed <abdelftahmohamed30@gmail.com>
2018-11-27 00:37:20 +0200

    Update

--- src/eg/edu/alexu/csd/oop/db/cs04/XML/Main.java
+++ src/eg/edu/alexu/csd/oop/db/cs04/XML/Main.java
@@ -1,7 +1,10 @@
 package eg.edu.alexu.csd.oop.db.cs04.XML;
 
 public class Main {
-    public static void main(String[] args) {
-        createTable ct = new createTable("table1","name","mark");
-    }
+	public static void main(String[] args) {
+		// createTable ct = new createTable("table1","name","mark");
+		Object[][] x = { { "column_name1", "column_name2", "column_name3" }, { "11111111", 22222222, "33333333" } };
+		Object[] cond = { "column_name3", "value3" };
+		Update up = new Update("table1", x, null);
+	}
 }
---
+++ src/eg/edu/alexu/csd/oop/db/cs04/XML/Update.java
@@ -0,0 +1,110 @@
+package eg.edu.alexu.csd.oop.db.cs04.XML;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class Update {
+
+	private String tableName;
+	private Object[][] update_value;
+	private Object[] condition;
+
+	public Update(String tableName, Object[][] update_value, Object[] condition) {
+		this.tableName = tableName;
+		this.condition = condition;
+		this.update_value = update_value;
+		update();
+	}
+
+	private void update() {
+
+		try {
+			String filepath = "dbs\\db1\\" + tableName + ".xml";
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+			Document doc = docBuilder.parse(filepath);
+
+			// Get the root element
+			NodeList rows = doc.getElementsByTagName("row");
+
+			// loop over all rows
+			for (int i = 0; i < rows.getLength(); i++) {
+				// get row from rows list.
+				Node nNode = rows.item(i);
+				// Check rows has elements.
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					// Access cols in this row.
+					Element col = (Element) nNode;
+
+					if (condition != null) {
+						updateWithCondition(col);
+					} else {
+						updateWithoutCondition(col);
+					}
+				}
+			}
+
+			// write the content into xml file
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new File(filepath));
+			transformer.transform(source, result);
+
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void updateWithoutCondition(Element col) {
+		// TODO Auto-generated method stub
+		for (int j = 0; j < update_value[0].length; j++) {
+			// Update cols.
+			col.getElementsByTagName(update_value[0][j].toString()).item(0)
+					.setTextContent(update_value[1][j].toString());
+		}
+	}
+
+	private void updateWithCondition(Element col) {
+		// TODO Auto-generated method stub
+		// Check update's condition.
+		if (col.getElementsByTagName(condition[0].toString()).item(0).getTextContent()
+				.equals(condition[1].toString())) {
+			// Loop over cols to update.
+			for (int j = 0; j < update_value[0].length; j++) {
+				// Update cols.
+				col.getElementsByTagName(update_value[0][j].toString()).item(0)
+						.setTextContent(update_value[1][j].toString());
+			}
+		}
+	}
+}
