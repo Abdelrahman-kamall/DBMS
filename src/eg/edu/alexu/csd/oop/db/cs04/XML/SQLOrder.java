1da955edd50794046be7807a87bb6e3890ecff95
Abdelfattah Mohamed <abdelftahmohamed30@gmail.com>
2018-12-02 21:46:58 +0200

    header

--- src/eg/edu/alexu/csd/oop/db/cs04/XML/SQLOrder.java
+++ src/eg/edu/alexu/csd/oop/db/cs04/XML/SQLOrder.java
@@ -6,95 +6,102 @@ import java.sql.SQLException;
 
 public class SQLOrder {
 
-    private boolean firstInstance = true;
-    private String database = null;
-    private String table_head = null;
-    private static final SQLOrder instance = new SQLOrder();
+	private boolean firstInstance = true;
+	private String database = null;
+	private String table_head = null;
+	private static final SQLOrder instance = new SQLOrder();
+	private String[] x;
 
-    private SQLOrder() {
-    }
+	private SQLOrder() {
+	}
 
-    public static SQLOrder getInstance() {
-        return instance;
-    }
+	public static SQLOrder getInstance() {
+		return instance;
+	}
 
-    public boolean createDatabase(String database) {
-        this.database = database;
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("dbs\\");
-        stringBuilder.append(database);
-        this.database = stringBuilder.toString();
-        createDataBase createDataBase = new createDataBase(this.database);
-        return createDataBase.isSuccess();
-    }
+	public boolean createDatabase(String database) {
+		this.database = database;
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("dbs\\");
+		stringBuilder.append(database);
+		this.database = stringBuilder.toString();
+		createDataBase createDataBase = new createDataBase(this.database);
+		return createDataBase.isSuccess();
+	}
 
-    public boolean dropDatabase(String database) {
-        try {
-        	StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append("dbs\\");
-            stringBuilder.append(database);
-            database=stringBuilder.toString();
-            deleteDataBase.deleteDirectoryStream(Paths.get(database));
-            this.database = null;
-            return true;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
+	public boolean dropDatabase(String database) {
+		try {
+			StringBuilder stringBuilder = new StringBuilder();
+			stringBuilder.append("dbs\\");
+			stringBuilder.append(database);
+			database = stringBuilder.toString();
+			deleteDataBase.deleteDirectoryStream(Paths.get(database));
+			this.database = null;
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 
-    public boolean createTable(String[][] input, String tablename){
-        if (database != null) {
-            return createTable.createTable(input, tablename,database);
-        }else {
-            return false;
-        }
+	public boolean createTable(String[][] input, String tablename) {
+		if (database != null) {
+			return createTable.createTable(input, tablename, database);
+		} else {
+			return false;
+		}
 
-    }
+	}
 
-    public boolean dropTable(String tablename) {
-        if (database != null) {
-            dropTable dropTable = new dropTable(tablename, database);
-            return dropTable.isSuccess();
-        }else {
-            return false;
-        }
-    }
+	public boolean dropTable(String tablename) {
+		if (database != null) {
+			dropTable dropTable = new dropTable(tablename, database);
+			return dropTable.isSuccess();
+		} else {
+			return false;
+		}
+	}
 
-    public int update(String tableName, Object[][] update_value, Object[][] condition) {
-        if(database!=null) {
-            Update update = new Update(tableName, update_value, condition, database);
-            return update.getCount();
-        }else {
-            return 0;
-        }
-    }
+	public int update(String tableName, Object[][] update_value, Object[][] condition) {
+		if (database != null) {
+			Update update = new Update(tableName, update_value, condition, database);
+			return update.getCount();
+		} else {
+			return 0;
+		}
+	}
 
-    public int insert(String name, String[][] cols) {
-        if(database!=null)
-        return InsertTable.insertRows(database+"\\"+name+".xml", name, cols);
-        else return 0;
-    }
+	public int insert(String name, String[][] cols) {
+		if (database != null)
+			return InsertTable.insertRows(database + "\\" + name + ".xml", name, cols);
+		else
+			return 0;
+	}
 
-    public int delete(String tableName, Object[][] condition) {
-        if(database!=null) {
-            Delete delete = new Delete(tableName, condition, database);
-            return delete.getcount();
-        }else {
-            return 0;
-        }
-    }
+	public int delete(String tableName, Object[][] condition) {
+		if (database != null) {
+			Delete delete = new Delete(tableName, condition, database);
+			return delete.getcount();
+		} else {
+			return 0;
+		}
+	}
 
-    public String getTable_head() {
-        return table_head;
-    }
+	public String getTable_head() {
+		return table_head;
+	}
 
-    public Object[][] select(String name, String[] cols, Object[][] condition) {
-        if(database!=null) {
-            table_head = database + "\\" + name + ".xml";
-            return selectTable.selectCols(database + "\\" + name + ".xml", name, cols, condition);
+	public Object[][] select(String name, String[] cols, Object[][] condition) {
+		x = cols;
+		if (database != null) {
+			table_head = database + "\\" + name + ".xml";
+			return selectTable.selectCols(database + "\\" + name + ".xml", name, cols, condition);
 
-        }
-        else return null;
-    }
+		} else
+			return null;
+	}
+
+	public String[] getX() {
+		return x;
+	}
 }
--- src/eg/edu/alexu/csd/oop/db/cs04/taskSplitter.java
+++ src/eg/edu/alexu/csd/oop/db/cs04/taskSplitter.java
@@ -9,49 +9,63 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class taskSplitter {
-    public static void parse(String input, Database database) throws SQLException {
-        input = input.toLowerCase();
-        String[] split = input.split(";");
-        for (String s : split) {
-            if(s.contains("create") || s.contains("drop")){
-                try {
-                    database.executeStructureQuery(s);
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }else if(s.contains("select")){
-                try {
-                    Object[][] o = database.executeQuery(s);
-                    String x = SQLOrder.getInstance().getTable_head();
-                    x = x.replace(".xml",".dtd");
-                    String[] strings = DTDGenerator.getDTDColumns(x);
-                    for (int j = 0; j < strings.length; j++) {
-                            System.out.printf("%-15s",strings[j]);
-                    }
-                    System.out.println();
-                    for(int i=0;i<o.length;i++) {
-                        for (int j = 0; j < o[0].length; j++) {
-                            if(o[i][j] instanceof Integer){
-                                System.out.printf("%-15d",o[i][j]);
-                            }else{
-                                System.out.printf("%-15s",o[i][j]);
-                            }
-                        }
-                        System.out.println();
-                    }
-                    //printSelect.print(database.executeQuery(s));
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }else if(s.contains("insert") || s.contains("delete") || s.contains("update")){
-                try {
-                    System.out.println(database.executeUpdateQuery(s));
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }else{
-                throw new SQLException("SYNTAX ERROR");
-            }
-        }
-    }
+	public static void parse(String input, Database database) throws SQLException {
+		input = input.toLowerCase();
+		String[] split = input.split(";");
+		for (String s : split) {
+			if (s.contains("create") || s.contains("drop")) {
+				try {
+					database.executeStructureQuery(s);
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			} else if (s.contains("select")) {
+				try {
+					Object[][] o = database.executeQuery(s);
+					String x = SQLOrder.getInstance().getTable_head();
+					x = x.replace(".xml", ".dtd");
+					String[] strings = DTDGenerator.getDTDColumns(x);
+					String[] cols = SQLOrder.getInstance().getX();
+					for (int i = 0; i < strings.length; i++) {
+						boolean flag = false;
+						for (int j = 0; j < cols.length; j++) {
+							if (strings[i].equals(cols[j])) {
+								flag = true;
+							}
+						}
+						if (!flag) {
+							strings[i] = null;
+						}
+					}
+					for (int j = 0; j < strings.length; j++) {
+						if (strings[j] != null) {
+							System.out.printf("%-15s", strings[j]);
+						}
+					}
+					System.out.println();
+					for (int i = 0; i < o.length; i++) {
+						for (int j = 0; j < o[0].length; j++) {
+							if (o[i][j] instanceof Integer) {
+								System.out.printf("%-15d", o[i][j]);
+							} else {
+								System.out.printf("%-15s", o[i][j]);
+							}
+						}
+						System.out.println();
+					}
+					// printSelect.print(database.executeQuery(s));
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			} else if (s.contains("insert") || s.contains("delete") || s.contains("update")) {
+				try {
+					System.out.println(database.executeUpdateQuery(s));
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			} else if(!s.contains("")) {
+				throw new SQLException("SYNTAX ERROR");
+			}
+		}
+	}
 }
