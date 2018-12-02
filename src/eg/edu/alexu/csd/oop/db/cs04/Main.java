81f584c62f0e47eedd045243a0f4adee6731952d
Abdelfattah Mohamed <abdelftahmohamed30@gmail.com>
2018-12-02 20:58:08 +0200

    1

--- src/eg/edu/alexu/csd/oop/db/cs04/Main.java
+++ src/eg/edu/alexu/csd/oop/db/cs04/Main.java
@@ -9,31 +9,31 @@ import eg.edu.alexu.csd.oop.db.cs04.taskSplitter;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Database db = new MyDatabase();
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNextLine()) {
-            String query = "";
-            Scanner lineScanner = new Scanner(scanner.nextLine());
+		Database db = new MyDatabase();
+		Scanner scanner = new Scanner(System.in);
+		while (scanner.hasNextLine()) {
+			String query = "";
+			Scanner lineScanner = new Scanner(scanner.nextLine());
 
-            while (lineScanner.hasNext()) {
-                String str = lineScanner.next();
-                if (str.toLowerCase().equals("exit")) {
-                    System.exit(0);
-                }
-                query += str + " ";
-            }
-            lineScanner.close();
-            try {
-                taskSplitter.parse(query, db);
-            } catch (SQLException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
+			while (lineScanner.hasNext()) {
+				String str = lineScanner.next();
+				if (str.toLowerCase().equals("exit")) {
+					System.exit(0);
+				}
+				query += str + " ";
+			}
+			lineScanner.close();
+			try {
+				taskSplitter.parse(query, db);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 
-        scanner.close();
+		scanner.close();
 
-    }
+	}
 }
